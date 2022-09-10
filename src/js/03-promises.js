e740534ff4f3fe607e70132ed4d64fde4cf60fe6
Sofya Omelyanyk <sofyaomelyanyk@gmail.com>
2022-09-10 16:27:06 +0300

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const { delay, step, amount } = e.target.elements;
+
+  const elems = {
+    delay: Number(delay.value),
+    step: Number(step.value),
+    amount: Number(amount.value),
+  };
+
+  let firstDelay = elems.delay;
+
+  for (let i = 1; i <= elems.amount; i += 1) {
+    createPromise(i, firstDelay).then(onSuccess).catch(onError);
+    firstDelay += elems.step;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSuccess({ position, delay }) {
+  console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+function onError({ position, delay }) {
+  console.log(`❌ Rejected promise ${position} in ${delay}ms`);
 }
